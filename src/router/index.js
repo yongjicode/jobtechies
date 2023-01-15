16a097b8969212259c6076806e78c25a821c384b
Chiraag Anand <chiraagsinghanand@gmail.com>
2023-01-15 21:47:05 +0800

    sign in page

--- src/router/index.js
+++ src/router/index.js
@@ -23,6 +23,15 @@ const routes = [
     },
   },
   {
+    path: "/signin",
+    name: "signin",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/SignInView.vue"),
+  },
+  {
     path: "/discover",
     name: "discover",
     // route level code-splitting
