73a1e35f22569a9740580467d0beb24741583685
Ng Yong Jie <nyjyongjie@gmail.com>
2023-01-15 20:09:03 +0800

    landing and home

---
+++ src/views/LandingView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <h1>THIS IS THE LANDING PAGE</h1>
+    <HelloWorld msg="LANDINGGGG to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "LandingView",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
