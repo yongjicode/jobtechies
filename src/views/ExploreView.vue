28f461aa85624e86b77788aceebcb150f5c5cf4b
Chiraag Anand <chiraagsinghanand@gmail.com>
2023-01-17 07:21:40 +0800

    explore tweaks

--- src/views/ExploreView.vue
+++ src/views/ExploreView.vue
@@ -2,13 +2,13 @@
   <div class="wrapper">
     <div class="image-container">
       <div
-        @click="setCurrentTrack(track)"
+        @click="setCurrentTrack(track.name)"
         v-for="track in tracks"
-        :key="track"
+        :key="track.name"
         class="circular-container"
       >
-        <img src="../assets/ai.png" class="circular-image" alt="image" />
-        <div class="title">{{ track }}</div>
+        <div :class="track.color" />
+        <div class="title">{{ track.name }}</div>
       </div>
     </div>
     <div class="screenshot-container">
@@ -42,18 +42,19 @@ export default {
     return {
       currentTrack: "DATA & AI",
       tracks: [
-        "DATA & AI",
-        "INFRASTRUCTURE",
-        "SOFTWARE",
-        "STRATEGY & GOVERNANCE",
-        "OPERATIONS & SUPPORT",
-        "CYBERSECURITY",
-        "SALES & MARKETING",
+        { name: "DATA & AI", color: "c-ai" },
+        { name: "INFRASTRUCTURE", color: "c-infra" },
+        { name: "SOFTWARE", color: "c-software" },
+        { name: "STRATEGY & GOVERNANCE", color: "c-strat" },
+        { name: "OPERATIONS & SUPPORT", color: "c-ops" },
+        { name: "CYBERSECURITY", color: "c-cyber" },
+        { name: "SALES & MARKETING", color: "c-sales" },
       ],
     };
   },
   methods: {
     setCurrentTrack(track) {
+      console.log(this.currentTrack);
       this.currentTrack = track;
     },
   },
@@ -62,6 +63,9 @@ export default {
 
 <style scoped>
 .image-container {
+  margin-top: 30px;
+  padding-top: 20px;
+  background-color: #f5f5f5;
   display: flex;
   flex-wrap: wrap;
   justify-content: space-between;
@@ -69,24 +73,67 @@ export default {
 
 .circular-container {
   position: relative;
-  height: 150px;
+  height: 50px;
   margin: 0 auto;
+  cursor: pointer;
 }
 
-.circular-image {
-  width: 100px;
-  height: 100px;
+.c-ai {
+  background-color: #056c9f;
+  width: 20px;
+  height: 20px;
   border-radius: 50%;
-  object-fit: cover;
+}
+
+.c-infra {
+  background-color: #01a8c8;
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+}
+
+.c-software {
+  background-color: #d63626;
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+}
+
+.c-ops {
+  background-color: #eb921b;
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+}
+
+.c-strat {
+  background-color: #008074;
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+}
+
+.c-cyber {
+  background-color: #653a94;
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+}
+
+.c-sales {
+  background-color: #ad1f8f;
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
 }
 
 .title {
-  position: absolute;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  text-align: center;
+  text-align: left;
   font-size: 0.8rem;
-  padding: 10px;
+  width: 100%;
+}
+
+.screenshot-container {
+  padding-top: 30px;
 }
 </style>
